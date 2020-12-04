a60a9dc54a881ff6ad07d27a53a6a755e58461fe
Chanwit Kaewkasi <chanwit@gmail.com>
2020-12-04 17:55:06 +0700

    add dev.sh script to help getting started the dev workflow

---
+++ dev.sh
@@ -0,0 +1,22 @@
+TAG=$(cat VERSION)
+K0S_VERSION=$(cat K0S_VERSION)
+
+mkdir build/
+cd build/
+git clone https://github.com/k0sproject/k0s
+cd k0s
+git checkout -b build $K0S_VERSION
+stg init
+stg import -t -s ../../patches/series
+
+# Workflow
+# ========
+# hack
+#   stg add
+#   stg refresh
+# or
+# hack
+#   commit
+#   stg uncommit
+# then
+#   stg export -n -p -d ../../patches/
