9260875893897f03017638fef0e34368255fc08f
Chanwit Kaewkasi <chanwit@gmail.com>
2020-12-04 15:56:33 +0700

    use stgit to apply patches

--- build.sh
+++ build.sh
@@ -1,17 +1,11 @@
-TAG=v1.18.9-eks-1-18-1.1
+TAG=$(cat VERSION)
 
 mkdir build/
 cd build/
 git clone https://github.com/k0sproject/k0s
 cd k0s
 git checkout -b build v0.8.1
-
-git apply ../../patches/001_dockerfile.patch
-git apply ../../patches/002_embedded-bins_Makefile.patch
-git apply ../../patches/003_pkg_constant.patch
+stg import -s ../../patches/series
 
 make EMBEDDED_BINS_BUILDMODE=fetch
-docker login quay.io -u $QUERY_USERNAME -p $QUERY_PASSWORD
 docker build -t quay.io/chanwit/ekz:$TAG .
-docker push quay.io/chanwit/ekz:$TAG
-docker logout
