a23107fc1fbffc240403414469cceae01179db13
Chanwit Kaewkasi <chanwit@gmail.com>
2021-04-01 19:20:09 +0700

    bump to build 5

--- cmd/ekz/create_cluster_ekz.go
+++ cmd/ekz/create_cluster_ekz.go
@@ -32,7 +32,7 @@ func createClusterEKZ() error {
 	case "v1.18.9-eks-1-18-1":
 		ekzImageBuild = "8"
 	case "v1.19.6-eks-1-19-1":
-		ekzImageBuild = "4"
+		ekzImageBuild = "5"
 	}
 
 	imageName := fmt.Sprintf("quay.io/ekz-io/ekz:%s.%s", eksdVersion, ekzImageBuild)
