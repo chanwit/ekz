9e38ff0fc819eb78dc1cc48e7449847182702fc8
Chanwit Kaewkasi <chanwit@gmail.com>
2021-05-07 23:38:20 +0700

    upgrade eks-d release 4 to kind

--- images/kind/build.sh
+++ images/kind/build.sh
@@ -2,13 +2,13 @@ case $1 in
   1.18)
     BUILD="0"
     BASE_IMAGE="kindest/node:v1.18.15@sha256:5c1b980c4d0e0e8e7eb9f36f7df525d079a96169c8a8f20d8bd108c0d0889cc4"
-    URL="https://distro.eks.amazonaws.com/kubernetes-1-18/kubernetes-1-18-eks-3.yaml"
+    URL="https://distro.eks.amazonaws.com/kubernetes-1-18/kubernetes-1-18-eks-4.yaml"
     ;;
 
   1.19)
     BUILD="0"
     BASE_IMAGE="kindest/node:v1.19.7@sha256:a70639454e97a4b733f9d9b67e12c01f6b0297449d5b9cbbef87473458e26dca"
-    URL="https://distro.eks.amazonaws.com/kubernetes-1-19/kubernetes-1-19-eks-3.yaml"
+    URL="https://distro.eks.amazonaws.com/kubernetes-1-19/kubernetes-1-19-eks-4.yaml"
     ;;
 esac
 
