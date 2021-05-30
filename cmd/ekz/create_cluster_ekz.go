e64c46914d406decfd0f20ba2fa9b83784c0876a
Chanwit Kaewkasi <chanwit@gmail.com>
2021-05-30 14:38:23 +0700

    cli: use new ekz image 1-19-4.6 1-20-1.3

--- cmd/ekz/create_cluster_ekz.go
+++ cmd/ekz/create_cluster_ekz.go
@@ -25,16 +25,14 @@ func getEKZNetworkName(containerId string) (string, error) {
 }
 
 func createClusterEKZ() error {
-	// build "8" is the latest stable provided by EKZ 1.18
-	// build "4" is the latest stable provided by EKZ 1.19
 	var ekzImageBuild string
 	switch eksdVersion {
 	case "v1.18.16-eks-1-18-5":
 		ekzImageBuild = "0"
 	case "v1.19.8-eks-1-19-4":
-		ekzImageBuild = "5"
+		ekzImageBuild = "6"
 	case "v1.20.4-eks-1-20-1":
-		ekzImageBuild = "2"
+		ekzImageBuild = "3"
 	}
 
 	imageName := fmt.Sprintf("quay.io/ekz-io/ekz:%s.%s", eksdVersion, ekzImageBuild)
