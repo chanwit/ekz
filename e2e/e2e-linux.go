014670d3a98728c5c184225749bd148a49bfb2f0
Chanwit Kaewkasi <chanwit@gmail.com>
2021-07-02 14:19:19 +0700

    update e2e

--- e2e/e2e-linux.go
+++ e2e/e2e-linux.go
@@ -4,13 +4,21 @@ import (
 	"github.com/chanwit/script"
 )
 
+const (
+	V1_18_Image = "v1.18.16-eks-1-18-6"
+	V1_19_Image = "v1.19.12-eks-1-19-5"
+	V1_20_Image = "v1.20.7-eks-1-20-2"
+
+	LastestImage = V1_20_Image
+)
+
 func main() {
 	script.Debug = true
 	for _, provider := range []string{"ekz", "kind"} {
 		for eksdVersion, expected := range map[string]string{
-			"v1.20": "v1.20.4-eks-1-20-1",
-			"v1.19": "v1.19.8-eks-1-19-4",
-			"v1.18": "v1.18.16-eks-1-18-5",
+			"v1.20": V1_20_Image,
+			"v1.19": V1_19_Image,
+			"v1.18": V1_18_Image,
 		} {
 			if err := script.Run("ekz", "create", "cluster", "--provider="+provider, "--eksd-version="+eksdVersion); err != nil {
 				panic(err)
