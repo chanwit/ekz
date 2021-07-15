dd393c9b2affaa032162a7a9c0bf1df0c8b678a8
Chanwit Kaewkasi <chanwit@gmail.com>
2021-07-15 23:52:28 +0700

    e2e: update images

--- e2e/e2e-linux.go
+++ e2e/e2e-linux.go
@@ -5,9 +5,9 @@ import (
 )
 
 const (
-	V1_18_Image = "v1.18.16-eks-1-18-6"
-	V1_19_Image = "v1.19.12-eks-1-19-5"
-	V1_20_Image = "v1.20.7-eks-1-20-2"
+	V1_18_Image = "v1.18.16-eks-1-18-7"
+	V1_19_Image = "v1.19.12-eks-1-19-6"
+	V1_20_Image = "v1.20.7-eks-1-20-3"
 
 	LastestImage = V1_20_Image
 )
