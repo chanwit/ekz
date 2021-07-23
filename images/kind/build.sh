57ecdc9a1594b724e6efe42b2bed745a1bb9ff69
Chanwit Kaewkasi <chanwit@gmail.com>
2021-07-23 19:44:20 +0700

    kind: add eks-d 1-21

--- images/kind/build.sh
+++ images/kind/build.sh
@@ -16,6 +16,12 @@ case $1 in
     BASE_IMAGE="kindest/node:v1.20.7@sha256:cbeaf907fc78ac97ce7b625e4bf0de16e3ea725daf6b04f930bd14c67c671ff9"
     URL="https://distro.eks.amazonaws.com/kubernetes-1-20/kubernetes-1-20-eks-3.yaml"
     ;;
+
+  1.21)
+    BUILD="0"
+    BASE_IMAGE="kindest/node:v1.21.1@sha256:69860bda5563ac81e3c0057d654b5253219618a22ec3a346306239bba8cfa1a6"
+    URL="https://distro.eks.amazonaws.com/kubernetes-1-21/kubernetes-1-21-eks-1.yaml"
+    ;;
 esac
 
 EKSD_YAML="eksd.yaml"
